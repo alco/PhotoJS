3ee18315ab8c655c0870d4d47ff598d5a486f00d
Alexei Sholik <alcosholik@gmail.com>
2011-03-17 22:53:51 +0200

    Add a luancher script invoke_js.py

--- README.md
+++ README.md
@@ -2,8 +2,17 @@ This is a collection of scripts for use with Photoshop.
 
 See [Adobe Photoshop Scripting][1] for more information.
 
+Some scripts require arguments. I've included a launcher script
+`invoke_js` written in python to be able to invoke scripts from the
+command line and supply any desired arguments to them.
+
 At the moment, there is no particular arrangement of pieces of code
 within this project. As the number of utilities grows, I'll try to group
 them into larger files if appropriate.
+
+For more information on each particular script, check its code for the
+description.
+
+The code can be used freely for any purpose. No warranties provided.
 
   [1]: http://www.adobe.com/devnet/photoshop/scripting.html
---
+++ invoke_js.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+"""A simple script to invoke a given javascript file within Adobe
+Photoshop environment"""
+
+import os
+import sys
+import appscript
+
+
+def main(path, args):
+    """The main function"""
+
+    ps_app = appscript.app('Adobe Photoshop CS4')
+
+    # If there are paths among arguments, convert them to absolute paths
+    normalized_args = []
+    for arg in args:
+        if os.path.exists(arg):
+            normalized_args.append(os.path.abspath(arg))
+        else:
+            normalized_args.append(arg)
+
+    ps_app.do_javascript(open(path, 'r').read(),
+                         with_arguments=normalized_args)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'Usage: invoke-js SCRIPT_NAME [arguments ...]'
+        exit(1)
+
+    main(sys.argv[1], sys.argv[2:])
